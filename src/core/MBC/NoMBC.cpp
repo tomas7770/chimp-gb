e6b8ae69dec4e6b237d55088ba4407f5336c4f44
tomas7770 <77364520+tomas7770@users.noreply.github.com>
2026-02-24 19:26:39 +0000

    fix: read 0xFF from non-ROM addresses when no MBC

--- src/core/MBC/NoMBC.cpp
+++ src/core/MBC/NoMBC.cpp
@@ -2,5 +2,9 @@
 
 uint8_t NoMBC::readByte(std::vector<uint8_t> &romData, uint16_t address)
 {
+    if (address > MBC::ROM_END)
+    {
+        return 0xFF;
+    }
     return romData.at(address);
 }
