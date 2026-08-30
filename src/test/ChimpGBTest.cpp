35fcf00fc1cdb3e8ae978d8e5b61d52d7123ca96
tomas7770 <77364520+tomas7770@users.noreply.github.com>
2026-08-30 01:49:41 +0100

    feat: basic executable for automated testing

---
+++ src/test/ChimpGBTest.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <fstream>
+#include "Gameboy.h"
+
+void printSerialByte(void *userdata, uint8_t serialByte)
+{
+    std::cout << serialByte;
+}
+
+int main(int argc, char *args[])
+{
+    // Read args
+    std::string filepath;
+    int runCycles;
+    SystemType systemType;
+    if (argc < 4)
+    {
+        std::cout << "Please provide a ROM file path, number of cycles to run, and system type." << std::endl;
+        return -1;
+    }
+
+    filepath = args[1];
+    try
+    {
+        runCycles = std::stoi(args[2]);
+        systemType = static_cast<SystemType>(std::stoi(args[3]));
+    }
+    catch (std::exception err)
+    {
+        std::cout << "Number of cycles and system type must be integers." << std::endl;
+        return -1;
+    }
+
+    // Load ROM
+    std::ifstream dataStream(filepath, std::ios::binary | std::ios::ate);
+    if (!dataStream.good())
+    {
+        std::cout << "Error loading requested ROM. Perhaps this file doesn't exist?" << std::endl;
+        return -1;
+    }
+    auto size = dataStream.tellg();
+    dataStream.seekg(0);
+
+    Cartridge cart = Cartridge(dataStream, size);
+    Gameboy *gameboy = new Gameboy(cart, false, systemType);
+    gameboy->setSerialCallback(printSerialByte, nullptr);
+    gameboy->simulateBootRom(false);
+    for (int i = 0; i < runCycles; i++)
+    {
+        gameboy->doCycle(false);
+    }
+
+    delete gameboy;
+
+    return 0;
+}
